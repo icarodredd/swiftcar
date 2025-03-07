405febcb842fc84b993cfac6adf0bfc5efcffb88
icarodredd <icarog854@gmail.com>
2025-03-07 10:00:25 -0300

    feat: finish header

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -3,11 +3,35 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 describe("Header", () => {
-  it("render the header", () => {
+  it("renders the header", () => {
     render(<Header />);
 
     const header = screen.getByText("swiftcar");
 
     expect(header).toBeInTheDocument();
+  });
+
+  it("renders the links for 'Antigos', 'Todos', 'Mais Vendidos'", () => {
+    render(<Header />);
+
+    const links = screen.getAllByRole("link");
+
+    expect(links).toHaveLength(3);
+
+    expect(links[0]).toHaveTextContent("Antigos");
+    expect(links[1]).toHaveTextContent("Todos");
+    expect(links[2]).toHaveTextContent("Mais Vendidos");
+  });
+
+  it("renders the buttons for 'Search', Cart', 'Customer' Icons", () => {
+    render(<Header />);
+
+    const buttons = screen.getAllByRole("button");
+
+    expect(buttons).toHaveLength(3);
+
+    expect(buttons[0]).toHaveAttribute("id", "search")
+    expect(buttons[1]).toHaveAttribute("id", "cart")
+    expect(buttons[2]).toHaveAttribute("id", "customer")
   });
 });
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+import customerIcon from "@/icons/Customer.png";
+import searchIcon from "@/icons/Search.png";
+import shoppingIcon from "@/icons/ShoppingCart.png";
+import { Button } from "../ui/button";
+
 export default function Header() {
   return (
-    <header>
-      <h1>swiftcar</h1>
-    </header>
+    <>
+      <header className="flex justify-between items-center p-4">
+        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-2xl">
+          swiftcar
+        </h1>
+        <div className="flex gap-12">
+          <Link href={"/"} className="text-sm font-medium leading-none">
+            Antigos
+          </Link>
+          <Link href={"/"} className="text-sm font-medium leading-none">
+            Todos
+          </Link>
+          <Link href={"/"} className="text-sm font-medium leading-none">
+            Mais Vendidos
+          </Link>
+        </div>
+        <div className="flex">
+          <Button id="search" size={"sm"} variant="ghost">
+            <Image src={searchIcon} alt="Search Icon" />
+          </Button>
+          <Button id="cart" size={"sm"} variant="ghost">
+            <Image src={shoppingIcon} alt="Cart Icon" />
+          </Button>
+          <Button id="customer" size={"sm"} variant="ghost">
+            <Image src={customerIcon} alt="Customer Icon" />
+          </Button>
+        </div>
+      </header>
+      <hr />
+    </>
   );
 }
