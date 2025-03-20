3c63439ee47af3e217db5f9f5327b6adafb3e8c0
icarodredd <icarog854@gmail.com>
2025-03-20 14:15:44 -0300

    feat: adds the first mocked cars

---
+++ cars.ts
@@ -0,0 +1,41 @@
+export const classicCars = [
+  {
+    id: "bristol-405",
+    name: "Bristol 405 Drophead Coupé",
+    manufacturer: "Bristol Cars",
+    productionYear: "1955–1958",
+    engine: "2.0L Bristol 6-cylinder",
+    horsepower: 105,
+    topSpeed: "167 km/h",
+    image: "bristol405.png",
+    description:
+      "O Bristol 405 Drophead Coupé é um clássico britânico elegante, com carroceria conversível e motor de 6 cilindros.",
+    price: 25000,
+  },
+  {
+    id: "citroen-ds",
+    name: "Citroën DS",
+    manufacturer: "Citroën",
+    productionYear: "1955–1975",
+    engine: "1.9L - 2.3L 4-cylinder",
+    horsepower: 75 - 141,
+    topSpeed: "185 km/h",
+    image: "citroen-ds.png",
+    description:
+      "O Citroën DS é um ícone francês conhecido pelo design futurista e pela suspensão hidropneumática inovadora.",
+    price: 80000,
+  },
+  {
+    id: "opel-commodore-b",
+    name: "Opel Commodore B",
+    manufacturer: "Opel",
+    productionYear: "1972–1977",
+    engine: "2.5L - 3.0L 6-cylinder",
+    horsepower: 115 - 160,
+    topSpeed: "190 km/h",
+    image: "opel-comodore.png",
+    description:
+      "O Opel Commodore B é um sedã esportivo alemão dos anos 70, combinando potência e conforto.",
+    price: 35000,
+  },
+];
