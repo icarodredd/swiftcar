1e66c1751ba5052aa79bbb25b11c3fc5a829a95d
icarodredd <icarog854@gmail.com>
2025-03-26 14:52:09 -0300

    feat: add home & tests, no redirect for now

---
+++ __tests__/Home.spec.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Home from "@/app/page";
+import { screen } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+import { StaticImageData } from "next/image";
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: ({ src, alt }: { src: string; alt: string }) => (
+    <img src={src} alt={alt} data-testid="image" />
+  ),
+}));
+
+jest.mock("../components/EmblaCarousel", () => ({
+  __esModule: true,
+  default: ({ slides }: { slides: StaticImageData[] }) => (
+    <div data-testid="carousel">
+      {slides.map((slide, index) => (
+        <img
+          key={index}
+          src={slide.src}
+          alt={`Slide ${index}`}
+          data-testid="embla-slide"
+        />
+      ))}
+    </div>
+  ),
+}));
+
+jest.mock("../components/ui/carousel", () => ({
+  __esModule: true,
+  Carousel: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+  CarouselContent: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+  CarouselItem: ({ children }: { children: React.ReactNode }) => (
+    <div className="carousel-item">{children}</div>
+  ),
+}));
+
+describe("Home", () => {
+  it("should render the top carousel", () => {
+    const { container } = render(<Home />);
+
+    const topCarousel = container.querySelector("#carousel-top");
+
+    const slides = topCarousel?.querySelectorAll('[data-testid="embla-slide"]');
+
+    expect(slides).toHaveLength(3);
+  });
+
+  it("should render the section of most selled cars", () => {
+    const { container } = render(<Home />);
+
+    expect(screen.getByText("MAIS VENDIDOS")).toBeInTheDocument();
+
+    const bestSellersCarousel = container.querySelector(
+      "#best-sellers-carousel"
+    );
+
+    const bestSellersSlides =
+      bestSellersCarousel?.querySelectorAll(".carousel-item");
+
+    expect(bestSellersSlides).toHaveLength(5);
+  });
+
+  it("should render the section of recently added cars", () => {
+    const { container } = render(<Home />);
+
+    const recentlyAddedCarousel = container.querySelector(
+      "#recently-added-carousel"
+    );
+
+    const recentlyAddedSlides =
+      recentlyAddedCarousel?.querySelectorAll(".carousel-item");
+
+    expect(recentlyAddedSlides).toHaveLength(5);
+  });
+
+  it("should render the bottom carousel", () => {
+    const { container } = render(<Home />);
+
+    const bottomCarousel = container.querySelector("#carousel-bottom");
+
+    const bottomSlides = bottomCarousel?.querySelectorAll(
+      '[data-testid="embla-slide"]'
+    );
+
+    expect(bottomSlides).toHaveLength(2);
+  });
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,117 @@
+"use client";
+
+import citroenImage from "@/assets/banners/citroen-ds-banner.png";
+import bristolImage from "@/assets/banners/volvo-p1800-banner.png";
+import opelImage from "@/assets/banners/opel-commodore-banner.png";
+import buyYourCar from "@/assets/banners/buy-your-car-banner.png";
+import economybanner from "@/assets/banners/economy-banner.png";
+import { EmblaOptionsType } from "embla-carousel";
+import EmblaCarousel from "@/components/EmblaCarousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { carsForSale } from "@/cars";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDESTOP = [citroenImage, bristolImage, opelImage];
+const SLIDESBOTTOM = [buyYourCar, economybanner];
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="p-4">
+      <section id="carousel-top" className="rounded-xl">
+        <EmblaCarousel slides={SLIDESTOP} options={OPTIONS} />
+      </section>
+
+      <div className="text-lg font-bold mb-4">MAIS VENDIDOS</div>
+
+      <div id="best-sellers-carousel">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {carsForSale.bestSellers.map((car) => (
+              <CarouselItem
+                key={car.id}
+                className="md:basis-1/2 lg:basis-1/4 mb-10 rounded-2xl"
+              >
+                <div className="relative shadow-lg rounded-2xl">
+                  <Image
+                    src={`/${car.image}`}
+                    alt=""
+                    className="rounded-2xl"
+                    width={700}
+                    height={700}
+                  />
+
+                  <div className="text-center h-2/8 absolute inset-x-0 bottom-0 bg-swift-white rounded-b-2xl">
+                    <div className="font-extrabold mt-4">{car.name}</div>
+                    <div>
+                      {car.price.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+
+      <div className="text-lg font-bold mb-4">ADICIONADOS RECENTEMENTE</div>
+
+      <div id="recently-added-carousel">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {carsForSale.recentlyAdded.map((car) => (
+              <CarouselItem
+                key={car.id}
+                className="md:basis-1/2 lg:basis-1/4 mb-10 rounded-2xl"
+              >
+                <div className="relative shadow-lg rounded-2xl">
+                  <Image
+                    src={`/${car.image}`}
+                    alt=""
+                    className="rounded-2xl"
+                    width={700}
+                    height={700}
+                  />
+
+                  <div className="text-center h-2/8 absolute inset-x-0 bottom-0 bg-swift-white rounded-b-2xl">
+                    <div className="font-extrabold mt-4">{car.name}</div>
+                    <div>
+                      {car.price.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+
+      <section id="carousel-bottom" className="rounded-xl">
+        <EmblaCarousel slides={SLIDESBOTTOM} options={OPTIONS} />
+      </section>
+    </main>
+  );
 }
